7ffcc21f75af6c99be6fe5485274b5c699e0cc24
Kamen <kamen.boev@gmail.com>
2018-09-27 15:41:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-var prevScrollpos = window.pageYOffset;
+let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-60px";
+    document.getElementById("navbar").style.top = "-70px";
   }
   prevScrollpos = currentScrollPos;
 }
+
+let switchKey = true;
+let url = 'url("computer0.jpg")';
+setInterval(function() {
+	if(switchKey) {
+		url = 'url("computer0.jpg")';
+	} else {
+		url = 'url("chess0.jpg")';
+	}
+	document.getElementById("welcome-container").style.backgroundImage = url;
+	switchKey = !switchKey;
+}, 3000);
