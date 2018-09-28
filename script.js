72e3830d8cf5f89f2bb36245ef4b9d643ceb2768
Kamen <kamen.boev@gmail.com>
2018-09-28 16:57:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// Hides the navigation on scrolldown
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+  if (prevScrollpos > currentScrollPos || currentScrollPos < 2) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-70px";
@@ -9,6 +10,7 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 }
 
+// Rotate background pic of Welcome section
 let switchKey = true;
 let url = 'url("computer0.jpg")';
 setInterval(function() {
@@ -20,3 +22,21 @@ setInterval(function() {
 	document.getElementById("welcome-container").style.backgroundImage = url;
 	switchKey = !switchKey;
 }, 3000);
+
+
+// Project tiles interaction
+/*
+let tiles = document.getElementsByClassName('project-tile');
+
+
+for(let i in tiles) {
+	let tile = tiles[i];
+	
+	tile.addEventListener('mouseover', function(event) {
+		event.target.children[0].style.display = 'none';
+		setTimeout(function() {
+			event.target.children[0].style.display = 'block';
+		},3000);
+	});
+}
+*/
