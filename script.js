cd13c06ff3f16c0f429b16c886628a57d4ed6fc8
Kamen <kamen.boev@gmail.com>
2018-10-04 17:25:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -24,19 +24,62 @@ setInterval(function() {
 }, 3000);
 
 
-// Project tiles interaction
-/*
-let tiles = document.getElementsByClassName('project-tile');
+// Thanks to @uchardon on CodePen for this code
+initslider();
+function initslider() {
+  const elPrevButton = document.querySelector('.slider #prev');
+  const elNextButton = document.querySelector('.slider #next');
+
+  const elImages = Array.from(document.querySelectorAll('.slider .image'));
+
+  let state = {
+    photo: 0
+  };
+
+  function send(event) {
+
+    const elActives = document.querySelectorAll('.slider [data-active]');
+
+    Array.from(elActives)
+      .forEach(el => el.removeAttribute('data-active'));
+
+    switch (event) {
+      case 'PREV':
+        state.photo--;
+        break;
+      case 'NEXT':
+        state.photo++;
+        break;
+      default:
+        state.photo = +event;
+        break;
+    }
+
+    var len = elImages.length;
+    // Loop Around
+    //state.photo = ( ( state.photo % len) + len ) % len;
+    state.photo = Math.max(0, Math.min( state.photo, len - 1) );
+
+    Array.from(document.querySelectorAll(`.slider [data-key="${state.photo}"]`))
+      .forEach( el => {
+      el.setAttribute('data-active', true);
+    });
 
 
-for(let i in tiles) {
-	let tile = tiles[i];
-	
-	tile.addEventListener('mouseover', function(event) {
-		event.target.children[0].style.display = 'none';
-		setTimeout(function() {
-			event.target.children[0].style.display = 'block';
-		},3000);
-	});
-}
-*/
+  }
+  elPrevButton.addEventListener('click', () => {
+    send('PREV');
+  });
+
+  elNextButton.addEventListener('click', () => {
+    send('NEXT');
+  });
+
+  const elStatus = Array.from(document.querySelectorAll('.slider .stat'));
+  elStatus.forEach( stat => { 
+    stat.addEventListener('click', () => {
+      send(stat.dataset.key);
+    });
+  });
+  send(0);
+};
